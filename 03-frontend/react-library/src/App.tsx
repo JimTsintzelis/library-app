3d2762e815f68cddcd39fe8bf75d6a701a4118d8
JimTsintzelis <101722795+JimTsintzelis@users.noreply.github.com>
2024-09-06 20:03:46 +0300

    Update App.tsx

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -11,18 +11,18 @@ export const App = () => {
   return (
     <div className='d-flex flex-column min-vh-100'>
       <Navbar />
-      <div className ='flex-grow-1'>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to= '/home'/>
-        </Route>
-        <Route path='/home'>
-        <Homepage />
-        </Route>
-        <Route path='/search'>
-          <SearchBooksPage />
-        </Route>
-      </Switch>
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home'>
+            <Homepage />
+          </Route>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+        </Switch>
       </div>
       <Footer />
     </div>
